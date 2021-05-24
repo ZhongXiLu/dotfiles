fea616c221e434a91926c03148fbc07e7b2e4954
ZhongXiLu <zhongxilu@hotmail.com>
2021-05-24 14:34:43 +0200

    Updated lf config

--- .config/lf/pv.sh
+++ .config/lf/pv.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-case "$1" in
+case $(basename $1) in
     *.tar*) tar tf "$1";;
     *.zip) unzip -l "$1";;
     *.pdf) pdftotext "$1" -;;
-    *) highlight -O ansi "$1";;
+    *.*) highlight -O ansi "$1";;
+    *) cat "$1";;
 esac
