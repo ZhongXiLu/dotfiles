d044431f0c9c964278201bc3dedbef51096d11f3
ZhongXiLu <zhongxilu@hotmail.com>
2021-05-28 19:04:45 +0200

    Updated htoprc + subl.sh

--- .config/yadm/bootstrap.d/subl.sh
+++ .config/yadm/bootstrap.d/subl.sh
@@ -8,6 +8,7 @@ MACOS_CONFIG_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User
 
 if [ "$SYSTEM_TYPE" = "Darwin" ]; then
     [[ -d "$MACOS_CONFIG_DIR" ]] && rm -rf "$MACOS_CONFIG_DIR"
+    [ -L "$MACOS_CONFIG_DIR" ] && rm "$MACOS_CONFIG_DIR"
     ln -s "$DOTS_CONFIG_DIR" "$MACOS_CONFIG_DIR"
 fi
 # TODO: linux and windows
