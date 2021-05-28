d84c177ec595d6b42023e1c731ceca2d611e4ec6
ZhongXiLu <zhongxilu@hotmail.com>
2021-05-28 21:28:50 +0200

    Added brew.sh and oh-my-zsh.sh + moved oh-my-zsh theme to .config

---
+++ .config/yadm/bootstrap.d/oh-my-zsh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+DOTS_THEME_FILE="$HOME/.config/oh-my-zsh/zhong.zsh-theme"
+OHMYZSH_THEME_FILE="$HOME/.oh-my-zsh/custom/themes/zhong.zsh-theme"
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+[ -L "$OHMYZSH_THEME_FILE" ] && rm "$OHMYZSH_THEME_FILE"
+ln -s "$DOTS_THEME_FILE" "$OHMYZSH_THEME_FILE"
+
+echo "Oh My Zsh ✅"
