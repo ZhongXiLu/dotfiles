34311fd934c001aeeebc38c35416d0b2c37914e6
ZhongXiLu <zhongxilu@hotmail.com>
2021-05-28 18:51:41 +0200

    Added init.sh

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+SYSTEM_TYPE=$(uname -s)
+
+if [ "$SYSTEM_TYPE" = "Darwin" ]; then
+    
+    if [ ! command -v brew >/dev/null 2>&1 ]; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "Homebrew ✅"
+    fi
+
+    if ! brew list yadm >/dev/null; then
+        brew install yadm
+        echo "yadm ✅"
+    fi
+
+    if [ ! -f "$HOME/.config/yadm/bootstrap" ]; then
+        yadm clone https://github.com/ZhongXiLu/dotfiles.git --no-bootstrap
+    fi
+
+    yadm bootstrap
+    yadm decrypt
+
+fi
