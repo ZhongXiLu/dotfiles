d96be4385527bd2234b6cd05f02a605665204cc2
ZhongXiLu <zhongxilu@hotmail.com>
2021-05-30 14:58:50 +0200

    Updated init.sh (install xcode cli tools)

--- init.sh
+++ init.sh
@@ -11,6 +11,11 @@ if [ "$SYSTEM_TYPE" = "Darwin" ]; then
         echo "Homebrew ✅"
     fi
 
+    if ! xcode-select --print-path &>/dev/null; then
+        xcode-select --install
+        echo "Xcode cli tools ✅"
+    fi
+
     if ! brew list yadm >/dev/null; then
         brew install yadm
         echo "yadm ✅"
