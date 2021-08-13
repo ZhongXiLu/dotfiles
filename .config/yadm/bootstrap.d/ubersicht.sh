0129d332f32613ba65808055df297d0bb54e6b6b
ZhongXiLu <zhongxilu@hotmail.com>
2021-08-13 21:43:06 +0200

    Updated simple-bar

--- .config/yadm/bootstrap.d/ubersicht.sh
+++ .config/yadm/bootstrap.d/ubersicht.sh
@@ -5,7 +5,7 @@ set -eu
 SYSTEM_TYPE=$(uname -s)
 
 SIMPLE_BAR_FOLDER="$HOME/Library/Application Support/Übersicht/widgets/simple-bar"
-CUSTOM_STYLES_FILE="$SIMPLE_BAR_FOLDER/lib/styles/custom-styles.js"
+SETTINGS_FILE="$SIMPLE_BAR_FOLDER/lib/settings.js"
 THEMES_FILE="$SIMPLE_BAR_FOLDER/lib/styles/themes.js"
 SPACEDUCK_THEME_FILE="$SIMPLE_BAR_FOLDER/lib/styles/themes/spaceduck.js"
 
@@ -14,8 +14,8 @@ if [ "$SYSTEM_TYPE" = "Darwin" ]; then
         git clone https://github.com/Jean-Tinland/simple-bar "$SIMPLE_BAR_FOLDER"
     fi
 
-    [[ -L "$CUSTOM_STYLES_FILE" || -f "$CUSTOM_STYLES_FILE" ]] && rm "$CUSTOM_STYLES_FILE"
-    ln "$HOME/.config/simple-bar/custom-styles.js" "$CUSTOM_STYLES_FILE"
+    [[ -L "$SETTINGS_FILE" || -f "$SETTINGS_FILE" ]] && rm "$SETTINGS_FILE"
+    ln "$HOME/.config/simple-bar/settings.js" "$SETTINGS_FILE"
     [[ -L "$THEMES_FILE" || -f "$THEMES_FILE" ]] && rm "$THEMES_FILE"
     ln "$HOME/.config/simple-bar/themes.js" "$THEMES_FILE"
     [[ -L "$SPACEDUCK_THEME_FILE" || -f "$SPACEDUCK_THEME_FILE" ]] && rm "$SPACEDUCK_THEME_FILE"
