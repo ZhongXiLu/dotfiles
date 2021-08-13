7aa6243ab2547dcdd691bfcadf58cf55e740814b
ZhongXiLu <zhongxilu@hotmail.com>
2021-08-13 20:13:02 +0200

    Fixed sdkman bootstrap script + updated aliases

--- .config/yadm/bootstrap.d/sdkman.sh
+++ .config/yadm/bootstrap.d/sdkman.sh
@@ -5,11 +5,7 @@ set -e
 DOTS_CONFIG_FILE="$HOME/.config/sdkman/config"
 SDKMAN_CONFIG_FILE="$HOME/.sdkman/etc/config"
 
-declare -a JAVA_CANDIDATES=(
-    "8.0.282.j9-adpt"
-    "11.0.10.j9-adpt"
-    "16.0.1.j9-adpt"
-)
+declare -a JAVA_VERSIONS=("8" "11" "16")
 
 if [ ! -d "$HOME/.sdkman" ]; then
     curl -s "https://get.sdkman.io?rcupdate=false" | bash
@@ -19,8 +15,15 @@ fi
 ln -s "$DOTS_CONFIG_FILE" "$SDKMAN_CONFIG_FILE"
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-for java_candidate in "${JAVA_CANDIDATES[@]}"; do
-    sdk install java "$java_candidate"
+for JAVA_VERSION in "${JAVA_VERSIONS[@]}"; do
+
+    # Check if java version is already installed, if not, install it
+    # Does NOT have to be latest version
+    if [[ ! $(sdk list java | grep -E "((local only)|(installed)).*$JAVA_VERSION[^|]*.hs-adpt") ]]; then
+        JAVA_VERSION_IDENTIFIER=$(sdk list java | grep -E "adpt.*$JAVA_VERSION[^|]*.hs-adpt" | tr -s ' ' | rev | cut -d ' ' -f 2 | rev)
+        sdk install java $JAVA_VERSION_IDENTIFIER
+    fi
+
 done
 
 
