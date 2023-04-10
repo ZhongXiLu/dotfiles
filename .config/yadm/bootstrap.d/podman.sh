560cbdfcf1bc0b2edbe09a4c354ea10f6b3c5957
ZhongXiLu <zhongxilu@hotmail.com>
2023-04-10 12:12:19 +0200

    Add podman bootstrap file

---
+++ .config/yadm/bootstrap.d/podman.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+podman machine init || true
+sudo podman-mac-helper install
+
+echo "If necessary, you can install your custom certificate by:"
+echo -e "
+    \033[33mpodman machine start
+    podman machine ssh
+    [core@localhost ~]$ sudo su -
+    [root@localhost ~]# cd /etc/pki/ca-trust/source/anchors
+    [root@localhost ~]# vi custom_cert.pem
+    # Copy certificate contents in here, before continuing
+    [root@localhost anchors]# update-ca-trust
+    [root@localhost anchors]# exit
+    [core@localhost ~]$ exit\033[0m
+"
+
+echo "podman ✅"
